b041a6be6b74f5e6c22ed769dfe3a2944a458439
acobb <aecobb53@gmail.com>
2022-12-22 19:07:23 -0700

    adding current build endpoint

--- main.py
+++ main.py
@@ -9,3 +9,10 @@ app = FastAPI()
 @app.get('/')
 async def root(requests: Request):
     return {'Hello': 'WORLD!'}
+
+@app.get('/current-builds')
+async def current_builds(requests: Request):
+    with open('old_class_builds.html', 'r') as hf:
+        builds = hf.read()
+    return builds
+
